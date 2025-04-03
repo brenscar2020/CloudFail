315cdb52061ee4f8ef775a1725d36e0e8b599783
SUKH-X <122980766+Sukhhacker@users.noreply.github.com>
2025-04-03 10:30:27 +0530

    Update DNSDumpsterAPI.py
    Create new wrapper that follow old output structure

--- DNSDumpsterAPI.py
+++ DNSDumpsterAPI.py
@@ -1,24 +1,19 @@
 """
-This is the (unofficial) Python API for dnsdumpster.com Website.
-Using this code, you can retrieve subdomains
-
+This is a wrapper for the official DNSDumpster.com API that maintains compatibility
+with the old unofficial API's output structure.
 """
 
-from __future__ import print_function
 import requests
-import re
-import sys
 import base64
-
-from bs4 import BeautifulSoup
-
+import json
+import sys
 
 class DNSDumpsterAPI(object):
+    """DNSDumpsterAPI Main Handler - Now using official API but maintaining old output structure"""
 
-    """DNSDumpsterAPI Main Handler"""
-
-    def __init__(self, verbose=False, session=None):
+    def __init__(self, api_key=None, verbose=False, session=None):
         self.verbose = verbose
+        self.api_key = api_key
         if not session:
             self.session = requests.Session()
         else:
@@ -28,98 +23,102 @@ class DNSDumpsterAPI(object):
         if self.verbose:
             print('[verbose] %s' % s)
 
-    def retrieve_results(self, table):
-        res = []
-        trs = table.findAll('tr')
-        for tr in trs:
-            tds = tr.findAll('td')
-            pattern_ip = r'([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})'
-            try:
-                ip = re.findall(pattern_ip, tds[1].text)[0]
-                domain = str(tds[0]).split('<br/>')[0].split('>')[1].split('<')[0]
-                header = ' '.join(tds[0].text.replace('\n', '').split(' ')[1:])
-                reverse_dns = tds[1].find('span', attrs={}).text
+    def _convert_api_response(self, api_data, domain):
+        """Convert the official API response to the old format"""
+        result = {
+            'domain': domain,
+            'dns_records': {
+                'dns': [],
+                'mx': [],
+                'txt': api_data.get('txt', []),
+                'host': []
+            },
+            'image_data': None,  # Not available in official API
+            'xls_data': None    # Not available in official API
+        }
 
-                additional_info = tds[2].text
-                country = tds[2].find('span', attrs={}).text
-                autonomous_system = additional_info.split(' ')[0]
-                provider = ' '.join(additional_info.split(' ')[1:])
-                provider = provider.replace(country, '')
-                data = {'domain': domain,
-                        'ip': ip,
-                        'reverse_dns': reverse_dns,
-                        'as': autonomous_system,
-                        'provider': provider,
-                        'country': country,
-                        'header': header}
-                res.append(data)
-            except:
-                pass
-        return res
+        # Process A records
+        for a_record in api_data.get('a', []):
+            host = a_record['host']
+            for ip_info in a_record['ips']:
+                result['dns_records']['dns'].append({
+                    'domain': host,
+                    'ip': ip_info['ip'],
+                    'reverse_dns': ip_info.get('ptr', ''),
+                    'as': ip_info.get('asn', ''),
+                    'provider': ip_info.get('asn_name', ''),
+                    'country': ip_info.get('country', ''),
+                    'header': ''  # Not available in official API
+                })
+                result['dns_records']['host'].append({
+                    'domain': host,
+                    'ip': ip_info['ip'],
+                    'reverse_dns': ip_info.get('ptr', ''),
+                    'as': ip_info.get('asn', ''),
+                    'provider': ip_info.get('asn_name', ''),
+                    'country': ip_info.get('country', ''),
+                    'header': ''  # Not available in official API
+                })
 
-    def retrieve_txt_record(self, table):
-        res = []
-        for td in table.findAll('td'):
-            res.append(td.text)
-        return res
+        # Process MX records
+        for mx_record in api_data.get('mx', []):
+            host = mx_record['host']
+            for ip_info in mx_record['ips']:
+                result['dns_records']['mx'].append({
+                    'domain': host,
+                    'ip': ip_info['ip'],
+                    'reverse_dns': ip_info.get('ptr', ''),
+                    'as': ip_info.get('asn', ''),
+                    'provider': ip_info.get('asn_name', ''),
+                    'country': ip_info.get('country', ''),
+                    'header': ''  # Not available in official API
+                })
 
+        # Process NS records (added to dns section in old format)
+        for ns_record in api_data.get('ns', []):
+            host = ns_record['host']
+            for ip_info in ns_record['ips']:
+                result['dns_records']['dns'].append({
+                    'domain': host,
+                    'ip': ip_info['ip'],
+                    'reverse_dns': ip_info.get('ptr', ''),
+                    'as': ip_info.get('asn', ''),
+                    'provider': ip_info.get('asn_name', ''),
+                    'country': ip_info.get('country', ''),
+                    'header': ''  # Not available in official API
+                })
+
+        return result
 
     def search(self, domain):
-        dnsdumpster_url = 'https://dnsdumpster.com/'
+        """Search for domain information using the official API"""
+        
+        if not self.api_key:
+            print("API key is required for the official DNSDumpster API", file=sys.stderr)
+            return None
 
-        req = self.session.get(dnsdumpster_url)
-        soup = BeautifulSoup(req.content, 'html.parser')
-        csrf_middleware = soup.findAll('input', attrs={'name': 'csrfmiddlewaretoken'})[0]['value']
-        self.display_message('Retrieved token: %s' % csrf_middleware)
+        api_url = f"https://api.dnsdumpster.com/domain/{domain}"
+        headers = {
+            "X-API-Key": self.api_key,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
+        }
 
-        cookies = {'csrftoken': csrf_middleware}
-        headers = {'Referer': dnsdumpster_url, 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
-        data = {'csrfmiddlewaretoken': csrf_middleware, 'targetip': domain, 'user': 'free'}
-        req = self.session.post(dnsdumpster_url, cookies=cookies, data=data, headers=headers)
-
-        if req.status_code != 200:
-            print(
-                "Unexpected status code from {url}: {code}".format(
-                    url=dnsdumpster_url, code=req.status_code),
-                file=sys.stderr,
-            )
-            return []
-
-        if 'There was an error getting results' in req.content.decode('utf-8'):
-            print("There was an error getting results", file=sys.stderr)
-            return []
-
-        soup = BeautifulSoup(req.content, 'html.parser')
-        tables = soup.findAll('table')
-
-        res = {}
-        res['domain'] = domain
-        res['dns_records'] = {}
-        res['dns_records']['dns'] = self.retrieve_results(tables[0])
-        res['dns_records']['mx'] = self.retrieve_results(tables[1])
-        res['dns_records']['txt'] = self.retrieve_txt_record(tables[2])
-        res['dns_records']['host'] = self.retrieve_results(tables[3])
-
-        # Network mapping image
         try:
-            tmp_url = 'https://dnsdumpster.com/static/map/{}.png'.format(domain)
-            image_data = base64.b64encode(self.session.get(tmp_url).content)
-        except:
-            image_data = None
-        finally:
-            res['image_data'] = image_data
+            req = self.session.get(api_url, headers=headers)
+            
+            if req.status_code != 200:
+                print(
+                    f"Unexpected status code from API: {req.status_code}",
+                    file=sys.stderr,
+                )
+                return None
 
-        # XLS hosts.
-        # eg. tsebo.com-201606131255.xlsx
-        try:
-            pattern = r'/static/xls/' + domain + '-[0-9]{12}\.xlsx'
-            xls_url = re.findall(pattern, req.content.decode('utf-8'))[0]
-            xls_url = 'https://dnsdumpster.com' + xls_url
-            xls_data = base64.b64encode(self.session.get(xls_url).content)
-        except Exception as err:
-            print(err)
-            xls_data = None
-        finally:
-            res['xls_data'] = xls_data
+            api_data = req.json()
+            self.display_message(f"Successfully retrieved data from official API for {domain}")
+            
+            # Convert to old format
+            return self._convert_api_response(api_data, domain)
 
-        return res
+        except Exception as e:
+            print(f"Error accessing API: {str(e)}", file=sys.stderr)
+            return None
